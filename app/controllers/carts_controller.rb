47e0e5783df3a1041d2186444e4ea883f6538279
Wei-Cheng Yang <rainyday309@gmail.com>
2015-11-19 00:53:07 +0800

    清空購物車功能

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,4 +5,13 @@ class CartsController < ApplicationController
 		@order = current_user.orders.build
 		@info = @order.build_info
 	end
+
+	def abort
+		current_cart.destroy
+		session[:cart_id] = nil
+		current_cart
+
+		redirect_to carts_path
+	end
+
 end

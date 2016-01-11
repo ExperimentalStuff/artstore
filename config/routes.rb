92a87f2ec7db1754dd03ec0654ff4aeb16fe1989
Wei-Cheng Yang <rainyday309@gmail.com>
2016-01-11 22:57:24 +0800

    temporary views for admin::products controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'admin/products#index'
+
   namespace :admin do
     resources :products
   end

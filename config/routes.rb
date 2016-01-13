83e4b4c5fbb569d023d1dfe3f0b9fba61477ec77
Wei-Cheng Yang <rainyday309@gmail.com>
2016-01-13 17:28:25 +0800

    add user management functionality

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
 
   # Example of regular route:
